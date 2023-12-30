d0394166b7dff22f1b373390150e8ddb1484eb29
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2023-12-30 17:59:48 +0330

    Add files via upload

---
+++ 04 - Dynamic Js Components/js/script.js
@@ -0,0 +1,42 @@
+const topics = [
+    "21-dropdowns.html",
+    "22-accordion-collapse.html",
+    "23-carousel.html",
+    "24-toasts.html",
+    "25-spinners.html",
+    "26-modals.html",
+    "27-popovers-tooltips.html",
+    "28-darkmode-toggle.html"
+]
+
+const container = document.querySelector('.container')
+
+for (let i = 0; i < topics.length; i += 3) {
+    let thisTopics = generateItem(topics[i])
+    thisTopics += generateItem(topics[i + 1])
+    thisTopics += generateItem(topics[i + 2])
+
+    container.innerHTML += `
+        <div class="row mt-5">${thisTopics}</div>
+    `
+}
+
+
+function generateItem(topic) {
+    return `
+        <div class="col-md-4">
+            <div class="card rounded-4 shadow-lg border-0 mb-3 px-2 py-3">
+                <div class="card-body">
+                    <div class="p-2">
+                        <h5 class="fw-bold">${topic.split('-').slice(1).join(' ').split('.')[0]}</h5>
+                    </div>
+                    <div class="d-grid">
+                        <a href="./${topic}">
+                            <button class="btn btn-dark rounded-4 my-2">Go to Projects</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+}
